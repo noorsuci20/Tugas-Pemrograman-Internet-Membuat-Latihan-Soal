8d3c552acfea63a2941723eb78201ea645bd78ee
noorsuci20 <106234014+noorsuci20@users.noreply.github.com>
2022-10-23 23:13:03 +0700

    Add files via upload

---
+++ Tugas 2/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container py-5">
+    <form action="" method="post">
+      <p>1. Seperangkat komponen yang saling berhubungan dan saling bekerjasama untuk mencapai bebera patujuan disebut…</p>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal1" id="soal1" value="Sistem">
+        <label class="form-check-label" for="soal1">
+        Sistem 
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal1" id="soal1" value="Informasi">
+        <label class="form-check-label" for="soal1">
+          Informasi
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal1" id="soal1" value="Data">
+        <label class="form-check-label" for="soal1">
+          Data
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal1" id="soal1" value="Pengetahuan">
+        <label class="form-check-label" for="soal1">
+          Pengetahuan
+        </label>
+      </div>
+      <p class="mt-3">2. Fakta mentah mengenai orang, tempat, kejadian , dan hal-hal yang penting bagi organisasi disebutdata disebut…</p>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal2" id="soal2" value="Sistem">
+        <label class="form-check-label" for="soal2">
+        Sistem 
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal2" id="soal2"value="Informasi">
+        <label class="form-check-label" for="soal2">
+          Informasi
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal2" id="soal2" value="Data">
+        <label class="form-check-label" for="soal2">
+          Data
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal2" id="soal2" value="Pengetahuan">
+        <label class="form-check-label" for="soal2">
+          Pengetahuan
+        </label>
+      </div>
+      <p class="mt-3">3. Fakta mentah mengenai orang, tempat, kejadian , dan hal-hal yang penting bagi organisasi disebutdata disebut…</p>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal3" id="soal3" value="Sistem Informasi">
+        <label class="form-check-label" for="soal3">
+        Sistem Informasi
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal3" id="soal3" value="Manajemen Sistem Informasi">
+        <label class="form-check-label" for="soal3">
+          Manajemen Sistem Informasi
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal3" id="soal3" value="Proses Pengembangan Sistem">
+        <label class="form-check-label" for="soal3">
+          Proses Pengembangan Sistem
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal3" id="soal3" value="Teknologi Informasi">
+        <label class="form-check-label" for="soal3">
+          Teknologi Informasi
+        </label>
+      </div>
+      <p class="mt-3">4. Data yang telah diproses atau diorganisasikan kembali menjadi sesuatu yang lebih bermakna disebutinformasi disebut...</p>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal4" id="soal4" value="Sistem">
+        <label class="form-check-label" for="soal4">
+        Sistem 
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal4" id="soal4" value="Informasi">
+        <label class="form-check-label" for="soal4">
+          Informasi
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal4" id="soal4" value="Data">
+        <label class="form-check-label" for="soal4">
+          Data
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal4" id="soal4" value="Pengetahuan">
+        <label class="form-check-label" for="soal4">
+          Pengetahuan
+        </label>
+      </div>
+      <div class="">
+      </div>
+      <p class="mt-3">5. Data dan informasi yang diolah lebih jauh lagi berdasarkan fakta, kebenaran, kepercayaan penerima disebut...</p>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal5" id="soal5" value="Sistem">
+        <label class="form-check-label" for="soal5">
+        Sistem 
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal5" id="soal5" value="Informasi">
+        <label class="form-check-label" for="soal5">
+          Informasi
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal5" id="soal5" value="Data">
+        <label class="form-check-label" for="soal5">
+          Data
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="soal5" id="soal5" value="Pengetahuan">
+        <label class="form-check-label" for="soal5">
+          Pengetahuan
+        </label>
+      </div>
+      <div class="">
+      <button class="btn btn-success mt-4" type="submit" name="submit" id="submit">Lihat Hasil</button>
+      </div>
+    </form>
+    <?php 
+        if (isset($_POST['submit'])){
+            $soal1 = $_POST['soal1'];
+            $soal2 = $_POST['soal2'];
+            $soal3 = $_POST['soal3'];
+            $soal4 = $_POST['soal4'];
+            $soal5 = $_POST['soal5'];
+            $correct =0;
+            $predikat = "";
+            $jawabanInput = array($soal1, $soal2, $soal3, $soal4, $soal5);
+            $jawaban = array("Sistem", "Data", "Manajemen Sistem Informasi", "Informasi", "Pengetahuan");
+            for ($i=0; $i < count($jawabanInput); $i++) { 
+                if ($jawabanInput[$i] == $jawaban[$i]) {
+                    $correct += 20;
+                  }
+            }
+            switch ($correct) {
+                case 100:
+                  $predikat = "Sangat Baik";
+                  break;
+                case 80:
+                  $predikat = "Baik";
+                  break;
+                case 60:
+                  $predikat = "Cukup";
+                  break;
+                case 40:
+                  $predikat = "Kurang";
+                  break;
+                case 20:
+                  $predikat = "Sangat Kurang";
+                  break;
+                  default:
+                  $predikat = "Semua Jawaban Salah";
+                  break;
+              }  
+              echo "<h4 class='text-primary mt-4'>Nilai anda: $correct</h4>";
+              echo "<h4 class='text-primary' >Keterangan: $predikat </h4>";
+              
+        } 
+        ?>
+  </div>
+</body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</html>
